0b4d5a6da83bc953023c06beb301dd7ddcdf1776
pburugupalli <prabhakar.burugupalli@infor.com>
2020-06-14 15:21:29 +0530

    Adding skeleton for backend spring project

---
+++ Server_Components/designer-stop-service/designer-stop-webapp/src/main/java/com/designerstop/server/designerstopwebapp/DesignerStopWebappApplication.java
@@ -0,0 +1,12 @@
+package com.designerstop.server.designerstopwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignerStopWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignerStopWebappApplication.class, args);
+	}
+}
---
+++ Server_Components/designer-stop-service/designer-stop-webapp/src/test/java/com/designerstop/server/designerstopwebapp/DesignerStopWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.designerstop.server.designerstopwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignerStopWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
